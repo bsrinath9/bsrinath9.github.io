b2e0e1069333988dff10db172a4dfb1f10af1e8b
Srinath B <54152067+bsrinath9@users.noreply.github.com>
2021-05-26 12:44:01 -0400

    Updates

---
+++ precache-manifest.5996904c96c2f956db9c62130772ccd4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f95a14366f056671c809273e6cab88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e80c137dea6ffff0f655",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80c137dea6ffff0f655",
+    "url": "/static/js/main.a8ddd91e.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "61ec52dca2675b4977b2352246eb6eaa",
+    "url": "/static/media/cts.61ec52dc.jpg"
+  },
+  {
+    "revision": "3162750e42b93ced2fada9b6aafe09ee",
+    "url": "/static/media/hcl.3162750e.png"
+  },
+  {
+    "revision": "3781be54f0fa9ff036d0ebddc0502c6e",
+    "url": "/static/media/resume.3781be54.pdf"
+  },
+  {
+    "revision": "5c30ec063d53a3aa70fba82783ef51fc",
+    "url": "/static/media/srinath.5c30ec06.jpg"
+  }
+]);
